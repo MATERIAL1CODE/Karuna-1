3c20e3fb90044546a82e8461f9604d9cbb9c40bc
GC-CODER1 <dgdproject1@gmail.com>
2025-06-19 16:57:21 +0530

    Updated _layout.tsx

--- app/(citizen)/_layout.tsx
+++ app/(citizen)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, History, User } from 'lucide-react-native';
+import { Chrome as Home, History, User } from 'lucide-react-native';
 
 export default function CitizenLayout() {
   return (
